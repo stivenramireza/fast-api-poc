f48cee377686da6d43adb950ebfbf8868f9b3fcf
Stiven Ramírez Arango <stivenramireza@gmail.com>
2020-08-25 16:00:13 -0500

    Path Parameters

--- main.py
+++ main.py
@@ -1,28 +1,50 @@
-from typing import Optional
 from fastapi import FastAPI
-from model import Item
+from enum_model import ModelName
 
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
+async def root():
     return {
-        "Hello": "World"
+        "message": "Hello World"
     }
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
+@app.get("/users/me")
+async def read_user_me():
     return {
-        "item_id": item_id,
-        "q": q
+        "user_id": "the current user"
     }
 
 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
     return {
-        "item_name": item.name,
-        "item_id": item_id
+        "user_id": user_id
+    }
+
+
+@app.get("/model/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {
+            "model_name": model_name,
+            "message": "Deep Learning FTW!"
+        }
+    if model_name.value == "lenet":
+        return {
+            "model_name": model_name,
+            "message": "LeCNN all the images"
+        }
+    return {
+        "model_name": model_name,
+        "message": "Have some residuals"
+    }
+
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {
+        "file_path": file_path
     }
